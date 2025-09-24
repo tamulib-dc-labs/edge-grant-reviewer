665c2299fa1758318dfda1e0c6ba4dfd9e4b98d2
Mark Baggett <markpbaggett@gmail.com>
2025-09-24 08:46:34 -0500

    Init with initial source code.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Edge Grant Reviewer
+
+A Website for Reviewing VTT files and Whisper X JSON generated for the Oral Histories Edge Grant.
---
+++ src/main.ts
@@ -0,0 +1,63 @@
+import { MediaFile } from "./types/media";
+import "./style.css";
+import "whisper-transcript-sticky";
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'whisper-transcript': any;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("./config.json")
+    .then((response) => response.json())
+    .then((mediaFiles: MediaFile[]) => {
+      const dropdown = document.createElement("select");
+      dropdown.id = "audio-dropdown";
+      dropdown.className = "border border-gray-300 rounded-lg p-2 my-4 font-serif";
+
+      function hasName(file: unknown): file is { name: string } {
+        return !!file && typeof file === 'object' && 'name' in file;
+      }
+
+      const validMediaFiles = mediaFiles.filter(hasName);
+
+      validMediaFiles.sort((a, b) => a.name.localeCompare(b.name));
+
+      validMediaFiles.forEach((file, index) => {
+        const option = document.createElement("option");
+        option.value = index.toString();
+        option.textContent = file.name;
+        dropdown.appendChild(option);
+      });
+
+      document.body.appendChild(dropdown);
+
+      let whisperElement = document.createElement("whisper-transcript");
+      document.body.appendChild(whisperElement);
+
+      const updateWhisperTranscript = (index: number) => {
+        const selectedFile = validMediaFiles[index];
+
+        document.body.removeChild(whisperElement);
+
+        whisperElement = document.createElement("whisper-transcript");
+        whisperElement.setAttribute("audio", selectedFile.audio);
+        whisperElement.setAttribute("url", selectedFile.url);
+
+        document.body.appendChild(whisperElement);
+      };
+
+      dropdown.addEventListener("change", (event) => {
+        const index = parseInt((event.target as HTMLSelectElement).value);
+        updateWhisperTranscript(index);
+      });
+
+      updateWhisperTranscript(0); // Set the first item as default
+    })
+    .catch((error) => {
+      console.error("Error loading media files:", error);
+    });
+});
